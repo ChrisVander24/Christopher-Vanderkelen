fe9a815cc621792ff29290b28b99f09e4e62fcd1
ChrisVander24 <vc47119@mail.harpercollege.edu>
2024-12-12 20:59:51 -0600

    Add files via upload

---
+++ Week3/PS3P4.py
@@ -0,0 +1,4 @@
+first_name = input("Enter your first name: ")
+steps_walked = int(input("Enter the number of steps: "))
+calories_burned = steps_walked * 0.25
+print(f"{first_name}, you burned {calories_burned} calories.")
